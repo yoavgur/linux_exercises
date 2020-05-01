c4042c6633b93d6f67a5757d70a46427dab5b1c8
yoga <yoavgurarieh@gmail.com>
2020-05-01 17:55:16 +0300

    small refactoring

--- ps.c
+++ ps.c
@@ -12,10 +12,11 @@
 #define CMDLINE_LENGTH 8
 #define STAT_LENGTH 5
 #define MAX_CMDLINE_LENGTH 70 // Just the max I want to print so it looks good :D
+#define MAX_PROC_NAME 17 // comm[16] in kernel + null terminator
 
 struct ProcessInfo {
 	int pid;
-	char name[16]; // comm[16] in kernel
+	char name[MAX_PROC_NAME]; 
 	char cmd_line[MAX_CMDLINE_LENGTH];
 } ;
 
@@ -24,7 +25,7 @@ int main(int argc, char *argv[]){
 	DIR *dr = opendir("/proc");
 	int pid;
 	
-	printf("PID\tNAME                    CMDLINE\n");
+	printf("PID\tNAME%-20sCMDLINE\n", " ");
 
 	if (dr == NULL){
 		perror("Couldn't open dir");
@@ -46,7 +47,7 @@ int print_line(int pid){
 	get_cmdline(&p);
 
 	if (strcmp(p.cmd_line, "") == 0){
-		printf("%d\t[%s]\n", pid, p.name);
+		printf("%d\t[%s]\n", pid, p.name); // supposedly a kernel thread...
 	} else {
 		printf("%d\t%-20s\t%s\n", pid, p.name, p.cmd_line);
 	}
@@ -84,12 +85,12 @@ int get_name(struct ProcessInfo *p){
 
 	fd = open(path, O_RDONLY);
 	if (fd == -1){
-		perror("Couldn't open file");
+		perror("Couldn't open stat");
 		return 1;
 	}
 
 	if (read(fd, buffer, 255) == -1){
-		perror("Coudln't open stat");
+		perror("Coudln't read from stat");
 		return 1;
 	}
 
@@ -103,7 +104,7 @@ int get_name(struct ProcessInfo *p){
 
 	memset(end_index, 0, 1);
 
-	strncpy(p->name, start_index+1, 16);
+	strncpy(p->name, start_index+1, MAX_PROC_NAME);
 	
 	return 0;
 }
